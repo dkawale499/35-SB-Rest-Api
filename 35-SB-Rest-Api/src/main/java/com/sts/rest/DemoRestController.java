a5ac92306e328a7b0cbd8a1e1078e45b87e4fcd3
dkawale499 <dkawale499@gmail.com>
2024-04-13 09:00:25 +0530

    Https Status Updated

--- 35-SB-Rest-Api/src/main/java/com/sts/rest/DemoRestController.java
+++ 35-SB-Rest-Api/src/main/java/com/sts/rest/DemoRestController.java
@@ -1,18 +1,44 @@
 package com.sts.rest;
 
+import java.time.LocalDate;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 //import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoRestController {
+	
+	public ResponseEntity<String>getMsg(){
+		String msg="Welcome to Rest API Dashbord";
+		return new ResponseEntity<>(msg, HttpStatus.OK);
+	}
+	
+	/*
+	@PostMapping("/date")
+	public String getDate() {
+		LocalDate now= LocalDate.now();
+		return String.valueOf(now);
+		}
+	*/
+	
+	@GetMapping("/ok/{name}")
+	public String getGreetMsg(@PathVariable("name")String name){
+		String msg= name+",Good Morning";
+		return msg;
+	}
 	@GetMapping("/welcome")
-	public String getWelcomeMsg() {
-		String msg ="Welcome to REST API";
+	public String getWelcomeMsg(@RequestParam(" name")String name) {
+		String msg =name+",Welcome to REST API";
 		return msg;
 	}
-	public String getGreetMsg() {
-		String msg="Good Morning";
+	@GetMapping("/pp")
+	public String getGreetMsg(){
+		String msg= ",Good Morning";
 		return msg;
-	}
-}
+}}
